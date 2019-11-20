52f1635a8f720e2701733e6970cd8b119572c807
John Bohannon <imjohnbo@github.com>
2019-11-19 21:13:56 -0500

    feat: improve example in index.js

--- app/templates/index.js
+++ app/templates/index.js
@@ -1,9 +1,13 @@
-'use strict';
 const alfy = require('alfy');
 
-alfy.output([
-	{
-		title: 'Unicorn',
-		subtitle: alfy.input
-	}
-]);
+const data = await alfy.fetch('https://jsonplaceholder.typicode.com/posts');
+
+const items = alfy
+    .inputMatches(data, 'title')
+    .map(element => ({
+        title: element.title,
+        subtitle: element.body,
+        arg: element.id
+    }));
+
+alfy.output(items);
